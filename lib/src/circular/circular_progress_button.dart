c3bf6ea1ab54fadb35bb20ed4f46ad5440e31dba
zhangyu <210014468@qq.com>
2024-08-19 15:17:26 +0800

    ✨ feat(封装波浪):

--- lib/src/circular/circular_progress_button.dart
+++ lib/src/circular/circular_progress_button.dart
@@ -4,50 +4,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-/// 进度条
 class CircularAnimatedProgressBar extends StatefulWidget {
-  /// 进度
-  late double progress;
-
-  /// 大小
-  late double size = 150;
-
-  /// 是否显示进度
-  late bool isShowProgress;
-
-  /// 是否显示进度的样式
-  late TextStyle showProgressTextStyle;
-
-  /// 构造方法
-  CircularAnimatedProgressBar(
-      {super.key,
-      this.size = 150.0,
-      this.progress = .2,
-      this.isShowProgress = true,
-      this.showProgressTextStyle = const TextStyle(
-          fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)})
-      : assert(progress >= 0.0 && progress <= 1.0,
-            "Progress must be less than 1 and greater than 0"),
-        assert(size > 0.0, "The size must be greater than 0");
-  @override
-  _CircularAnimatedProgressBarState createState() =>
-      _CircularAnimatedProgressBarState();
-}
-
-class _CircularAnimatedProgressBarState
-    extends State<CircularAnimatedProgressBar> {
-  @override
-  Widget build(BuildContext context) {
-    return WaterWaveProgressBar(
-      size: widget.size,
-      progress: widget.progress,
-      isShowProgress: widget.isShowProgress,
-      showProgressTextStyle: widget.showProgressTextStyle,
-    );
-  }
-}
-
-class WaterWaveProgressBar extends StatefulWidget {
   /// 大小
   final double size;
 
@@ -59,22 +16,57 @@ class WaterWaveProgressBar extends StatefulWidget {
 
   /// 是否显示进度的样式
   final TextStyle showProgressTextStyle;
-  const WaterWaveProgressBar(
+
+  /// 波浪高度
+  final double waveHeight;
+
+  /// 动画曲线
+  final Curve curve;
+
+  /// 波浪颜色&波浪层数
+  /// 默认为蓝色
+  /// 2、4、6 排序显示为 最上层为 6 、4 、2
+  late List<Color> colorsWave;
+
+  /// 构造方法
+  CircularAnimatedProgressBar(
       {super.key,
-      required this.size,
-      required this.progress,
-      required this.isShowProgress,
-      required this.showProgressTextStyle});
+      this.size = 150.0,
+      this.progress = .2,
+      this.isShowProgress = true,
+      this.curve = Curves.linear,
+      this.waveHeight = 12,
+
+      /// 默认为蓝色
+      /// 2、4、6 排序显示为 最上层为 6 、4 、2
+      this.colorsWave = const [
+        Color(0x1A2196f3),
+        Color(0x662196f3),
+        Color(0xCC2196f3)
+      ],
+      this.showProgressTextStyle = const TextStyle(
+          fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)})
+      : assert(progress >= 0.0 && progress <= 1.0,
+            "Progress must be less than 1 and greater than 0"),
+        assert(size > 0.0, "The size must be greater than 0");
 
   @override
-  _WaterWaveProgressBarState createState() => _WaterWaveProgressBarState();
+  _CircularAnimatedProgressBarState createState() =>
+      _CircularAnimatedProgressBarState();
 }
 
-class _WaterWaveProgressBarState extends State<WaterWaveProgressBar>
-    with TickerProviderStateMixin {
+class _CircularAnimatedProgressBarState
+    extends State<CircularAnimatedProgressBar> with TickerProviderStateMixin {
+  /// 进度动画控制器
   late AnimationController _progressController;
+
+  /// 波浪动画控制器
   late AnimationController _waveController;
+
+  /// 进度动画
   late Animation<double> _progressAnimation;
+
+  /// 波浪动画
   late double _oldProgress;
 
   @override
@@ -90,7 +82,7 @@ class _WaterWaveProgressBarState extends State<WaterWaveProgressBar>
 
     _progressAnimation =
         Tween<double>(begin: _oldProgress, end: widget.progress).animate(
-      CurvedAnimation(parent: _progressController, curve: Curves.linear),
+      CurvedAnimation(parent: _progressController, curve: widget.curve),
     );
 
     // 水波纹动画控制器，保持循环
@@ -101,13 +93,13 @@ class _WaterWaveProgressBarState extends State<WaterWaveProgressBar>
   }
 
   @override
-  void didUpdateWidget(WaterWaveProgressBar oldWidget) {
+  void didUpdateWidget(CircularAnimatedProgressBar oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.progress != widget.progress) {
       _oldProgress = oldWidget.progress;
       _progressAnimation =
           Tween<double>(begin: _oldProgress, end: widget.progress).animate(
-        CurvedAnimation(parent: _progressController, curve: Curves.linear),
+        CurvedAnimation(parent: _progressController, curve: widget.curve),
       );
 
       // 开始执行进度动画
@@ -135,7 +127,10 @@ class _WaterWaveProgressBarState extends State<WaterWaveProgressBar>
                 height: widget.size,
                 child: CustomPaint(
                   painter: MultiLayerWaterWavePainter(
-                      _waveController.value, _progressAnimation.value),
+                      waveAnimationValue: _waveController.value,
+                      progress: _progressAnimation.value,
+                      waveHeight: widget.waveHeight,
+                      colorsWave: widget.colorsWave),
                 )),
             widget.isShowProgress
                 ? Text('${(_progressAnimation.value * 100).toInt()}%',
@@ -149,14 +144,27 @@ class _WaterWaveProgressBarState extends State<WaterWaveProgressBar>
 }
 
 class MultiLayerWaterWavePainter extends CustomPainter {
+  /// 水波纹动画值
   final double waveAnimationValue;
+
+  /// 进度
   final double progress;
 
-  MultiLayerWaterWavePainter(this.waveAnimationValue, this.progress);
+  /// 波浪高度
+  final double waveHeight;
+
+  /// 波浪颜色&波浪层数
+  /// 默认为蓝色
+  /// 2、4、6 排序显示为 最上层为 6 、4 、2
+  final List<Color> colorsWave;
+  MultiLayerWaterWavePainter(
+      {required this.waveAnimationValue,
+      required this.progress,
+      required this.waveHeight,
+      required this.colorsWave});
 
   @override
   void paint(Canvas canvas, Size size) {
-    double waveHeight = 10.0;
     double waveLength = size.width;
     double radius = size.width / 2;
     double centerY = size.height * (1 - progress);
@@ -165,11 +173,11 @@ class MultiLayerWaterWavePainter extends CustomPainter {
     Paint wavePaint1 = Paint()..color = Colors.blue.withOpacity(0.4);
     Paint wavePaint2 = Paint()..color = Colors.blue.withOpacity(0.6);
     Paint wavePaint3 = Paint()..color = Colors.blue.withOpacity(0.8);
-
+    Paint wavePaint4 = Paint()..color = Colors.red.withOpacity(0.8);
     Path wavePath1 = Path();
     Path wavePath2 = Path();
     Path wavePath3 = Path();
-
+    Path wavePath4 = Path();
     // 第一层波浪
     for (double i = -waveLength / 4; i <= waveLength * 1.5; i++) {
       double dx = i;
@@ -214,6 +222,21 @@ class MultiLayerWaterWavePainter extends CustomPainter {
       }
     }
 
+    // 第三层波浪
+    for (double i = -waveLength / 4; i <= waveLength * 1.25; i++) {
+      double dx = i;
+      double dy = sin((i / waveLength * 1.5 * pi) +
+                  (waveAnimationValue * 2 * pi) +
+                  pi) *
+              waveHeight +
+          centerY;
+      if (i == -waveLength / 4) {
+        wavePath4.moveTo(dx, dy);
+      } else {
+        wavePath4.lineTo(dx, dy);
+      }
+    }
+
     wavePath1.lineTo(size.width * 3, size.height);
     wavePath1.lineTo(-waveLength / 4, size.height);
     wavePath1.close();
@@ -225,6 +248,10 @@ class MultiLayerWaterWavePainter extends CustomPainter {
     wavePath3.lineTo(size.width * 3, size.height);
     wavePath3.lineTo(-waveLength / 4, size.height);
     wavePath3.close();
+
+    wavePath4.lineTo(size.width * 3, size.height);
+    wavePath4.lineTo(-waveLength / 4, size.height);
+    wavePath4.close();
 
     // 绘制圆形背景
     canvas.drawCircle(Offset(radius, radius), radius, circlePaint);
@@ -238,6 +265,8 @@ class MultiLayerWaterWavePainter extends CustomPainter {
     canvas.drawPath(wavePath1, wavePaint1);
     canvas.drawPath(wavePath2, wavePaint2);
     canvas.drawPath(wavePath3, wavePaint3);
+
+    canvas.drawPath(wavePath4, wavePaint4);
   }
 
   @override
